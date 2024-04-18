a798d5263f2d0d6b85e5cc9df5c36640936e506d
Aron Dennen <aron@arondennen.com>
2024-04-17 22:48:52 -0400

    Update clock_divider.v

--- src/clock_divider.v
+++ src/clock_divider.v
@@ -1,10 +1,7 @@
 // Clock divider module, Aron Dennen 2024
 
-// Inputs a 12.5 MHz signal and outputs a 480 Hz and 60 Hz clock signals
+// Inputs a 12.5 MHz signal and outputs a 122400 Hz (480Hz * 255 Pwm periods/clock) and 60 Hz clock signal
 // Outputs toggle at twice the output frequency to maintin N rising edges per second
-
-// 12.5 MHz / 26042 = 479.99 hz
-// toggle every 13021 clocks
 
 module clock_divider (
   // Inputs
@@ -12,51 +9,54 @@ module clock_divider (
   input wire reset,
   
   // Outputs
-  output wire clk480,
+  output wire clkPwm,
   output wire clk60
 );
 
-  reg [13:0] count; // 14-bit counter for 12.5 MHz -> 480 Hz signal
-  reg [2:0] count2; // 3-bit counter for 480 Hz -> 60 Hz signal
+  reg [5:0] count; // 6-bit counter for 12.5 MHz -> 122,400 Hz signal (51 counts per transition)
+  reg [9:0] count2; // 10-bit counter for 122,400 Hz -> 60 Hz signal (1020 counts per transition)
 
-  reg out480;
+  reg outPwm;
   reg out60;
 
-  assign clk480 = out480;
+  assign clkPwm = outPwm;
   assign clk60 = out60;
 
   initial begin
-    count = 0;
-    count2 = 0;
+    count <= 0;
+    count2 <= 0;
 
-    out480 = 0;
-    out60 = 0;
+    outPwm <= 0;
+    out60 <= 0;
   end
 
   always @(posedge clk, posedge reset) begin
     if (reset) begin
       count <= 0;
-      out480 <= 0;
+      outPwm <= 0;
     end
     else begin
       count <= count + 1;
-      if (count == 13021) begin // 13021
+      if (count == 51) begin
         // reset the main counter
         count <= 0; 
-        out480 <= ~out480;
+        outPwm <= ~outPwm;
       end
     end
   end
 
-  always @(posedge out480, posedge reset) begin
+  always @(posedge outPwm, posedge reset) begin
     if (reset) begin
       count2 <= 0;
       out60 <= 0;
     end
     else begin
       count2 <= count2 + 1;
-      // counter2 self-resets because it's 3 bits counting to 8
-      if (count2 == 3'b000) out60 <= ~out60;
+
+      if (count2 == 1020) begin
+        count2 <= 0; 
+        out60 <= ~out60;
+      end
     end
   end
 
