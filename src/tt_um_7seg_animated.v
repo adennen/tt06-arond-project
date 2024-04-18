d33dcafa0b3be8e50ccf8aae94c24f947939f88b
Aron Dennen <aron@arondennen.com>
2024-04-17 22:56:46 -0400

    Update and rename tt_um_7seghex_animated.v to tt_um_7seg_animated.v

--- src/tt_um_7seg_animated.v
+++ src/tt_um_7seg_animated.v
@@ -50,14 +50,9 @@ module tt_um_7seghex_animated (
 
   assign uo_out = { displayOut, 1'b0 };
 
-
-
   // *** Main program
+  assign charInput = ui_in[6:0];
 
-  always @(posedge reset) begin
-    // Dummy info
-    charInput <= ui_in[6:0];
-  end
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
