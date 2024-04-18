df22d178e9dbb4fa7269198615bd489f55a64467
Aron Dennen <aron@arondennen.com>
2024-04-18 07:53:41 -0400

    Update tt_um_7seg_animated.v

--- src/tt_um_7seg_animated.v
+++ src/tt_um_7seg_animated.v
@@ -49,8 +49,9 @@ module tt_um_7seg_animated (
   always @(posedge clk) begin
     reset <= ~rst_n;
     charInput <= ui_in[6:0];
-    uo_out = { displayOut, 1'b0 };
   end
+    
+  assign uo_out = { displayOut, 1'b0 };
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
