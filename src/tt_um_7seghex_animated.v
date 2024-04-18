9d1adff0430331bc04fe70af820a887b70324ed2
Aron Dennen <aron@arondennen.com>
2024-04-17 21:34:20 -0400

    Update tt_um_7seghex_animated.v

--- src/tt_um_7seghex_animated.v
+++ src/tt_um_7seghex_animated.v
@@ -16,6 +16,7 @@ module tt_um_7seghex_animated (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  reg reset;
   assign reset = ~rst_n;
 
   
@@ -27,7 +28,7 @@ module tt_um_7seghex_animated (
   clock_divider clk_divider (
     .clk(clk),
     .reset(reset),
-    .clk480(clk480),
+    //.clk480(clk480),
     .clk60(clk60)
     );
 
@@ -56,11 +57,12 @@ module tt_um_7seghex_animated (
 
   always @(posedge reset) begin
     // Dummy info
-    charInput = ui_in[6:0];
+    charInput <= ui_in[6:0];
   end
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_in = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
