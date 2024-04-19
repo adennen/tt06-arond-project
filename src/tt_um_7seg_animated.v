c49c37b0991f7903a810cec6cdd44b877b84326f
Aron Dennen <aron@arondennen.com>
2024-04-19 11:09:19 -0400

    Update tt_um_7seg_animated.v

--- src/tt_um_7seg_animated.v
+++ src/tt_um_7seg_animated.v
@@ -64,13 +64,14 @@ module tt_um_7seg_animated (
     reset <= ~rst_n;
     charInput <= ui_in[6:0];
 
-     if (usePwm)
-        uo_out[6:0] = (pwmOut) ? displayOut : 7'b0000000;
-     else uo_out[6:0] = displayOut;
+    if (usePwm) begin
+      uo_out[6:0] <= (pwmOut) ? displayOut : 7'b0000000;
+    end
+    else uo_out[6:0] <= displayOut;
   end
 
   assign usePwm = uio_in[7];
-  assign uo_out[7] = { ui_in[7], displayOut };
+  assign uo_out[7] = ui_in[7];
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
